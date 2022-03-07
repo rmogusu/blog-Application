50e90b8c367e7859c60edabf003c6b1d6320ef31
Rose Mogusu <rmogusu@hq.ekenya.co.ke>
2022-03-07 12:22:35 +0300

    Implement get all posts method

--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -19,46 +19,46 @@ public class PostController {
         this.postService = postService;
     }
 
-//    // create blog post rest api
-//    @PostMapping("/api/posts")
-//    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
-//        return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
-//    }
-//
-//    // get all posts rest api
-//    @GetMapping("/api/posts")
-//    public PostResponse getAllPosts(
-//            @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
-//            @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
-//            @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
-//            @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir
-//    ){
-//        return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
-//    }
-//
-//    // get post by id
-//    @GetMapping(value = "/api/posts/{id}")
-//    public ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") long id) {
-//        return ResponseEntity.ok(postService.getPostById(id));
-//    }
-//
-////    @ApiOperation(value = "Update Post By Id REST API")
-////    @PreAuthorize("hasRole('ADMIN')")
-//    // update post by id rest api
-//    @PutMapping("/api/posts/{id}")
-//    public ResponseEntity<PostDto> updatePost(@RequestBody PostDto postDto, @PathVariable(name = "id") long id){
-//
-//        PostDto postResponse = postService.updatePost(postDto, id);
-//
-//        return new ResponseEntity<>(postResponse, HttpStatus.OK);
-//    }
-//    // delete post rest api
-//    @DeleteMapping("/api/posts/{id}")
-//    public ResponseEntity<String> deletePost(@PathVariable(name = "id") long id){
-//
-//        postService.deletePostById(id);
-//
-//        return new ResponseEntity<>("Post entity deleted successfully.", HttpStatus.OK);
-//    }
+    // create blog post rest api
+    @PostMapping("/api/posts")
+    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
+        return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+    }
+
+    // get all posts rest api
+    @GetMapping("/api/posts")
+    public PostResponse getAllPosts(
+            @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir
+    ){
+        return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
+    }
+
+    // get post by id
+    @GetMapping(value = "/api/posts/{id}")
+    public ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") long id) {
+        return ResponseEntity.ok(postService.getPostById(id));
+    }
+
+//    @ApiOperation(value = "Update Post By Id REST API")
+//    @PreAuthorize("hasRole('ADMIN')")
+    // update post by id rest api
+    @PutMapping("/api/posts/{id}")
+    public ResponseEntity<PostDto> updatePost(@RequestBody PostDto postDto, @PathVariable(name = "id") long id){
+
+        PostDto postResponse = postService.updatePost(postDto, id);
+
+        return new ResponseEntity<>(postResponse, HttpStatus.OK);
+    }
+    // delete post rest api
+    @DeleteMapping("/api/posts/{id}")
+    public ResponseEntity<String> deletePost(@PathVariable(name = "id") long id){
+
+        postService.deletePostById(id);
+
+        return new ResponseEntity<>("Post entity deleted successfully.", HttpStatus.OK);
+    }
 
 }
