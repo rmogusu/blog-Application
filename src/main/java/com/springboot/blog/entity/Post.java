95840a876ad02c2b4c31d7f6927533de7c6ce1c0
Rose Mogusu <rmogusu@hq.ekenya.co.ke>
2022-03-07 12:23:58 +0300

    Implement delete posts method

--- src/main/java/com/springboot/blog/entity/Post.java
+++ src/main/java/com/springboot/blog/entity/Post.java
@@ -15,16 +15,16 @@ import javax.persistence.*;
         name = "posts", uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})}
 )
 public class Post {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(name = "title", nullable = false)
-    private String title;
-
-    @Column(name = "description", nullable = false)
-    private String description;
-
-    @Column(name = "content", nullable = false)
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @Column(name = "title", nullable = false)
+//    private String title;
+//
+//    @Column(name = "description", nullable = false)
+//    private String description;
+//
+//    @Column(name = "content", nullable = false)
     private String content;
 }
