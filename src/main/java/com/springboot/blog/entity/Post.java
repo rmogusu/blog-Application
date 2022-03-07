e719552c42c9566449e7db9de7a11aab27302d3c
Rose Mogusu <rmogusu@hq.ekenya.co.ke>
2022-03-07 08:43:48 +0300

    Add spring lombok annotations.

--- src/main/java/com/springboot/blog/entity/Post.java
+++ src/main/java/com/springboot/blog/entity/Post.java
@@ -1,22 +1,30 @@
 package com.springboot.blog.entity;
 
 
-import ch.qos.logback.classic.db.names.ColumnName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "post", uniqueConstraints = {uniqueConstrait(columnNames = {"title"})})
+@Table(
+        name = "posts", uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})}
+)
 public class Post {
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "title", nullable = false)
     private String title;
+
+    @Column(name = "description", nullable = false)
     private String description;
+
+    @Column(name = "content", nullable = false)
     private String content;
 }
