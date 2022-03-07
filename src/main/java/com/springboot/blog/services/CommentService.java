590e4c050f2ab8af386e62d97216eccbece1a27e
Rose Mogusu <rmogusu@hq.ekenya.co.ke>
2022-03-07 12:39:46 +0300

    Add comment servece interface to define crud method for comments

--- src/main/java/com/springboot/blog/services/CommentService.java
+++ src/main/java/com/springboot/blog/services/CommentService.java
@@ -1,4 +1,13 @@
 package com.springboot.blog.services;
 
-public class CommentService {
+public interface CommentService {
+    CommentDto createComment(long postId, CommentDto commentDto);
+
+    List<CommentDto> getCommentsByPostId(long postId);
+
+    CommentDto getCommentById(Long postId, Long commentId);
+
+    CommentDto updateComment(Long postId, long commentId, CommentDto commentRequest);
+
+    void deleteComment(Long postId, Long commentId);
 }
